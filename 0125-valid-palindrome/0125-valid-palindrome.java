cf50d3d7cf8234f1e22ca90b9256d862212ab3d3
Amresh Chaubey <iamresh7355@gmail.com>
2026-09-14 21:36:34 +0530

    Time: 4 ms (44.19%), Space: 44.7 MB (42.16%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+       
+        StringBuilder sb = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            if (Character.isLetterOrDigit(c)) {
+                sb.append(Character.toLowerCase(c));
+            }
+        }
+
+        // Step 2: Use two‑pointer technique to check palindrome
+        int left = 0, right = sb.length() - 1;
+        while (left < right) {
+            if (sb.charAt(left) != sb.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
+
+    
